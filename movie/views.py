7676596edacc5fb6a3176bd88c7e9fca957a30dd
Daniel Gonzalez <dgonza45@eafit.edu.co>
2022-08-02 10:25:34 -0500

    Version final

--- movie/views.py
+++ movie/views.py
@@ -5,6 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    #return HttpResponse('<h1> Welcome to Home Page </h1>')
-    #return render(request, 'home.html')
+    # return HttpResponse('<h1> Welcome to Home Page </h1>')
+    # return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'Daniel Gonzalez'})
+
+
+def about(request):
+    return render(request, 'about.html', {'name': 'Daniel Gonzalez'})
